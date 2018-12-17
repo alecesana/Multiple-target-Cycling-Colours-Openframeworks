672f4290ddcf118292d0aeb557b76101f2a5ac6b
Ale Cesana <39651347+alecesana@users.noreply.github.com>
2018-12-17 22:42:16 +1100

    Update MultiTargetColourCycling.cpp
    neated up code

--- src/MultiTargetColourCycling.cpp
+++ src/MultiTargetColourCycling.cpp
@@ -7,17 +7,13 @@ ColourSfumatore::ColourSfumatore() {
 void ColourSfumatore::setup(int _hexcolour1, int _hexcolour2, int _hexcolour3, int _nobjects) { 
 
 	timeconstant = 1;
-	nobjects = _nobjects;
+	nobjects = _nobjects;	
 	
-	
-	// colors we are cycling through
+	// assigning html colors we are cycling through to swapping color objects
 
-
-	c0.setHex(_hexcolour1,200); //cc
-	c1.setHex(_hexcolour2,200); //cc
-	c2.setHex(_hexcolour3,200); //cc
-
-	//colourcube[0], colourcube[1], colourcube[2] = _hexcolour1;
+	c0.setHex(_hexcolour1,200); 
+	c1.setHex(_hexcolour2,200); 
+	c2.setHex(_hexcolour3,200); 
 	
 	groupNobjects = nobjects / nmaincolors;
 
@@ -25,12 +21,8 @@ void ColourSfumatore::setup(int _hexcolour1, int _hexcolour2, int _hexcolour3, i
 
 //--------------------------------------------------------------
 void ColourSfumatore::update() {
-
-
 	
 	colorintime = fmodf(timeconstant* ofGetElapsedTimef(), 1);
-
-
 	//assigning three rotating blending colours to the three ofColor variables c0,c1,2
 	c0toc1 = c0;//assign c0
 	c0toc1.lerp(c1, colorintime); 
@@ -44,40 +36,32 @@ void ColourSfumatore::update() {
 	c2toc0.lerp(c0, colorintime);
 	c[2] = c2toc0;
 
-
-
-	if ((ofGetElapsedTimef() - colortimer >= 0) && (ofGetElapsedTimef() - colortimer < timeconstant * 1)) {
-	
+	if ((ofGetElapsedTimef() - colortimer >= 0) && (ofGetElapsedTimef() - colortimer < timeconstant * 1)) {	
 	
 			colourcube[0] = c[0];
 			colourcube[1] = c[1];
-			colourcube[2] = c[2];
-			
+			colourcube[2] = c[2];			
 	}
 
 	if ((ofGetElapsedTimef() - colortimer >= timeconstant * 1) && (ofGetElapsedTimef() - colortimer < timeconstant * 2)) {
-
-
-	
+		
 			colourcube[0] = c[1];
 			colourcube[1] = c[2];
 			colourcube[2] = c[0];
 		
-
 	}
 	if ((ofGetElapsedTimef() - colortimer >= timeconstant * 2) && (ofGetElapsedTimef() - colortimer < timeconstant * 3)) {
 
 			colourcube[0] = c[2];
 			colourcube[1] = c[0];
-			colourcube[2] = c[1];
-		
+			colourcube[2] = c[1];		
 
 	}
 
 	//wrapping back to starting colour c0
 
 	if (ofGetElapsedTimef() - colortimer >= timeconstant * 3) {
-	
+		
 		colourcube[0] = c[0];
 		colourcube[1] = c[1];
 		colourcube[2] = c[2];
@@ -85,40 +69,29 @@ void ColourSfumatore::update() {
 		colortimer = ofGetElapsedTimef();
 
 	}
-
-
 }
 
 //--------------------------------------------------------------
 void ColourSfumatore::draw(int _counter) {
-
 	
 	counter = _counter;
-
 	if ((counter >= 0) && (counter < groupNobjects)) {
-	
 		
 		ofSetColor(colourcube[0]);
-
 		
 	}
-	else if ((counter >= groupNobjects) && (counter < 2 * groupNobjects)){
+	else if ((counter >= groupNobjects) && (counter < 2 * groupNobjects)){		
 		
-		
-		ofSetColor(colourcube[1]);
-	
+		ofSetColor(colourcube[1]);	
 		
 	}
-	else if ((counter >= 2 * groupNobjects) && (counter < nobjects))
-
-	{	
+	else if ((counter >= 2 * groupNobjects) && (counter < nobjects)){	
 
 		ofSetColor(colourcube[2]);
 
 	}
-
 	
-	}
+}
 
 
 
